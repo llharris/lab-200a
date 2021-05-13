475e73c6d6f51a01900c5398790f76e45541300c
Lee Harris <lee.mark.harris@gmail.com>
2021-05-13 14:31:12 +0000

    Updates

--- terraform/vsphere.tf
+++ terraform/vsphere.tf
@@ -6,6 +6,16 @@ terraform {
   }
 }
 
+data "vsphere_datacenter" "datacenter" {
+  name = "Datacenter"
+}
+
+data "vsphere_distributed_virtual_switch" "dvs" {
+  name          = "DSwitch"
+  datacenter_id = "datacenter-3"
+}
+
+
 provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
@@ -13,12 +23,11 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-resource "vsphere_datacenter" "prod_datacenter" {
-  name = "myDC"
+resource "vsphere_distributed_port_group" "pg" {
+  name                            = "terraform-test-pg"
+  distributed_virtual_switch_uuid = "data.vsphere_distributed_virtual_switch.dvs.id"
 }
 
-
-
-resource "vsphere_distributed_port_group" "pg" {
-  name = "test-portgroup"
-  distributed_virtual_switch_
+output "datacenter_id" {
+  value = data.vsphere_datacenter.datacenter.id
+}
